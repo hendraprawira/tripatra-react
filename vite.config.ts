f082c9afceb32e3620a138862ff4cfc5f8e56ad8
Hendra Prawira <prawirah@gmail.com>
2022-06-28 03:03:32 +0700

    update env

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/gql' : {
-        target: 'https://tripatra-procurement.herokuapp.com',
+        target: 'https://tripatra-procurement.herokuapp.com/graphql',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/gql/, '')
       }
