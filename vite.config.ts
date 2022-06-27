ea50a0672f69e2beffa0b84ac9606eeae7ed2442
Hendra Prawira <prawirah@gmail.com>
2022-06-28 02:05:41 +0700

    config backend

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/gql' : {
-        target: 'http://localhost:3000',
+        target: 'https://tripatra-procurement.herokuapp.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/gql/, '')
       }
